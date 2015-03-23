bb8572d444c4ee1b4630b5ebfb392f9904049c38
dude56987 <dude56987@users.noreply.github.com>
2015-03-23 16:29:45 -0400

    Pull settings from settings file into interface

--- web/systemSettings.php
+++ web/systemSettings.php
@@ -1,6 +1,11 @@
 <html>
 	<head>
 		<title></title>
+	<style>
+		input{
+			width:100%;
+		}	
+	</style>
 	</head>
 	<body>
 		<?PHP
@@ -8,7 +13,7 @@
 		function grabInput($varName){
 			$input=filter_input(INPUT_GET, $varName);
 			return $input;
-		}
+		};
 		$settings= array(
 			"sourceLocation" => "",
 			"styleLocation" => "",
@@ -16,34 +21,24 @@
 			"breakfastStart" => "",
 			"lunchStart" => "",
 			"dinnerStart" => "",
-			"lateMealStart" => "",
-		)
+			"lateMealStart" => ""
+		);
 		for($index=0;$index>count($settings);$index++){
 			$settings[$index]=grabInput($settings[$index]);
-		}
+		};
 		//else show the form
 		// readfile /etc/glue.cfg and input current values into below
 		// form so user can update and rewrite the file
-		print "
-		<form action='systemSettings.php'>
-			<h2>SourceData Location</h2>
-			<input name='sourceLocation' type='text' value='' />
-			<h2>StyleSheet Location</h2>
-			<input name='styleLocation' type='text' value='' />
-			<h2>Output Location</h2>
-			<input name='outputLocation' type='text' value='' />
-			<h2>Breakfast Start Time</h2>
-			<input name='breakfastStart' type='number' value='' />
-			<h2>Lunch Start Time</h2>
-			<input name='lunchStart' type='number' value='' />
-			<h2>Dinner Start Time</h2>
-			<input name='dinnerStart' type='number' value='' />
-			<h2>Late Meal Start Time</h2>
-			<input name='lateMealStart' type='number' value='' />
-			<p>	
-			<input type='submit' value='Save Changes' />
-			</p>
-		</form>";
+		//
+		//Read the file to insert it below
+		$configFile=file("/etc/glue.cfg");
+		print "<form action='systemSettings.php'>";
+		foreach($configFile as $entry){
+			$entry = explode('=',$entry);
+			print "<h2>".$entry[0]."</h2>";
+			print "<input name='outputLocation' type='text' value='".$entry[1]."' />";
+		}
+		print "<p><input type='submit' value='Save Changes' /></p></form>";
 		?>
 	</body>
 </html>
