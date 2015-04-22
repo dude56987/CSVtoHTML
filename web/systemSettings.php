5843dbb96b19fc37e7d8fea56416027969f22a39
dude56987 <dude56987@users.noreply.github.com>
2015-04-22 15:46:03 -0400

    Made systemSettings file editing more dynamic

--- web/systemSettings.php
+++ web/systemSettings.php
@@ -11,71 +11,49 @@
 	<body>
 		<?PHP
 		// if the user has submitted the form to this page do work
-		function grabInput($varName){
-			$input=filter_input(INPUT_GET, $varName);
-			return $input;
-		}
 		function writeFile($filePath,$stringToWrite){
 			 $myFileObject = fopen($filePath, "w") or die("Unable to open file!");
 			 fwrite($myFileObject, $stringToWrite);
 			 fclose($myFileObject);
 		}
-		$settings= array(
-			"location" => "location",
-			"stylesheetLocation" => "stylesheetLocation",
-			"outputLocation" => "outputLocation",
-			"breakfastStartTime" => "breakfastStartTime",
-			"lunchStartTime" => "lunchStartTime",
-			"dinnerStartTime" => "dinnerStartTime",
-			"lateMealStartTime" => "lateMealStartTime"
-		);
-		//for($index=0;$index<count($settings);$index++){
-		foreach($settings as $item){
-			$temp=grabInput($item);
-			if(count($temp)){
-				// concat the name and the value given to look like
-				// the config file so the same code can read the 
-				// config file into the program
-				$settings[$item]=$settings[$item].'='.$temp;
-			}
+		function cleanLineEnds($string){
+			// converts all line ends to \n line ends
+			return str_replace(array("\r\n", "\r"), "\n", $string);
 		}
-		// if the length of the sourcelocation variable is longer than
-		// the string source variable itself, a new setting is being
-		// saved and it needs to be updated
-		if (strlen($settings['location']) > (strlen("location"))){
-			print "<h3 style='color:green'>Updated Savefile</h3>";
-			$configFile=$settings;
-			// pull the configfile into a string
-			$tempArray = ''; 
-			foreach($configFile as $item){
-				$tempArray=$tempArray.$item."\n";
-			}
-			// write the new version of the configfile
-			writeFile('/etc/glue.cfg',$tempArray);
-			// update the glue config
-			exec('touch /tmp/refreshGlue');
-		}else{
+		$fileContent="";
+		$configFile= array();
+		// read glue.cfg and write back changes if there are any
+		foreach(file("/etc/glue.cfg") as $item){
+			// replace line endings with \n
+			$item = str_replace(array("\r\n", "\r","\n"), "\n", $item);
 			// readfile /etc/glue.cfg and input current values into below
 			// form so user can update and edit the file
-			$configFile=file("/etc/glue.cfg");
-			// remove line endings
-			$tempArray=array();
-			foreach($configFile as $entry){
-				// replace line endings
-				$entry = str_replace(array("\r\n", "\r","\n"), "", $entry);
-				// append the array
-				array_push($tempArray, $entry);
+			if($item[0]=="#"){
+				// re-add comments back to the file
+				$fileContent=$fileContent.$item;
+			}elseif(stristr($item,"=")){
+				$item=explode("=",$item);
+				// build a new config file
+				// add each element to fileContent
+				$fileContent=$fileContent.$item[0]."=".$item[1];
+				// add each config option element to an array
+				$configFile[$item[0]]=$item[1];
 			}
-			$configFile=$tempArray;
 		}
 		// begin building the html to be shown on the webpage
-		print "<form action='systemSettings.php'>";
-		foreach($configFile as $entry){
-			if($entry[0] != "#"){
-				// split up lines based on '='
-				$entry = explode('=',$entry);
-				print "<h2>".$entry[0]."</h2>";
-				print "<input name='".$entry[0]."' type='text' value='".$entry[1]."' />";
+		print "<form method='POST' action='writeSystemSettings.php'>";
+		foreach(explode("\n",$fileContent) as $entry){
+			if(! empty($entry)){
+				if($entry == "##"){
+					print "<hr />";
+				}elseif($entry[0] == "#"){
+					print "<p>".$entry."</p>";
+				}else{
+					// split up lines based on '='
+					$entry = explode('=',$entry);
+					print "<h2 style='color:green;'>".$entry[0]."</h2>";
+					print "<input name='".$entry[0]."' type='text' value='".$entry[1]."' /><br />";
+				}
 			}
 		}
 		print "<p><input type='submit' action='systemSettings.php' value='Save Changes' /></p></form>";
---
+++ web/writeSystemSettings.php
@@ -0,0 +1,46 @@
+<?PHP
+// if the user has submitted the form to this page do work
+function writeFile($filePath,$stringToWrite){
+	 $myFileObject = fopen($filePath, "w") or die("Unable to open file!");
+	 fwrite($myFileObject, $stringToWrite);
+	 fclose($myFileObject);
+}
+function cleanLineEnds($string){
+	// converts all line ends to \n line ends
+	return str_replace(array("\r\n", "\r"), "\n", $string);
+}
+if(count($_POST)){ 
+	$fileContent="";
+	$configFile= array();
+	// read glue.cfg and write back changes if there are any
+	foreach(file("/etc/glue.cfg") as $item){
+		// replace line endings with \n
+		$item = str_replace(array("\n\n","\r\n", "\r"), "\n", $item);
+		// readfile /etc/glue.cfg and input current values into below
+		// form so user can update and edit the file
+		if($item[0]=="#"){
+			// re-add comments back to the file
+			$fileContent=$fileContent.$item;
+		}elseif(stristr($item,"=")){
+			$item=explode("=",$item);
+			// build a new config file
+			// add each element to fileContent
+			$fileContent=$fileContent.$item[0]."=".$_POST[$item[0]]."\n";
+		}
+	}
+	print $fileContent."<br />";//DEBUG
+	// if the length of the sourcelocation variable is longer than
+	// the string source variable itself, a new setting is being
+	// saved and it needs to be updated
+	// if the user has submited anything write the new stuff to the file
+	print "<h3 style='color:green'>Updated Savefile</h3>";
+	// write the new version of the configfile
+	writeFile('/etc/glue.cfg',$fileContent);
+	// update the glue config
+	exec('touch /tmp/refreshGlue');
+}
+?>
+<script>
+	window.location="systemSettings.php";
+</script>
+<a href="systemSettings.php">Keep Going</a>
